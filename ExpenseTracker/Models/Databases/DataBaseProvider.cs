6cd2bb7ee1e5acfa42b8f601f2c8e23bd558d661
melnyk-a <melnyk.an.il@gmail.com>
2018-08-15 19:30:22 +0300

    close #3

---
+++ ExpenseTracker/Models/Databases/DataBaseProvider.cs
@@ -0,0 +1,83 @@
+﻿using ExpenseTracker.Models.BasicIdentities;
+using ExpenseTracker.Models.Databases;
+using System;
+using System.Collections.Generic;
+
+namespace ExpenseTracker.Models
+{
+    internal class DatabaseProvider<T> : 
+        IDatabaseProvider<T> 
+        where T : BasicIdentity
+    {
+        private readonly IList<T> items = new List<T>();
+
+        public IList<T> Items => items;
+
+        public event EventHandler<DataItemEventArgs<T>> DatabaseChanged;
+
+        public void AddItem(T item)
+        {
+            items.Add(item);
+            item.IdentityChanged += new EventHandler<IdentityPropertyChangedEventArgs>(OnItemChanged);
+            OnDataBaseChange(new DataItemEventArgs<T>(item, ChangedAction.Add));
+        }
+
+        public bool Contains(string itemName)
+        {
+            bool isContain = false;
+
+            foreach (T item in items)
+            {
+                if (item.Name.Equals(itemName))
+                {
+                    isContain = true;
+                    break;
+                }
+            }
+
+            return isContain;
+        }
+
+        public void DeleteItem(T item)
+        {
+            foreach (T currentItem in items)
+            {
+                if (currentItem.Name.Equals(item.Name))
+                {
+                    if (items.Remove(currentItem))
+                    {
+                        OnDataBaseChange(new DataItemEventArgs<T>(currentItem, ChangedAction.Remove));
+                        item.IdentityChanged -= new EventHandler<IdentityPropertyChangedEventArgs>(OnItemChanged);
+                    }
+                    break;
+                }
+            }
+        }
+
+        public T Find(string itemName)
+        {
+            T findedAccount = null;
+
+            foreach (T account in items)
+            {
+                if (account.Name.Equals(itemName))
+                {
+                    findedAccount = account;
+                    break;
+                }
+            }
+
+            return findedAccount;
+        }
+
+        private void OnDataBaseChange(DataItemEventArgs<T> e)
+        {
+            DatabaseChanged?.Invoke(this, e);
+        }
+
+        private void OnItemChanged(object sender, IdentityPropertyChangedEventArgs e)
+        {
+            OnDataBaseChange(new DataItemEventArgs<T>((T)sender, ChangedAction.Update));
+        }
+    }
+}
---
+++ ExpenseTracker/Models/Databases/IDataBaseProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpenseTracker.Models.Databases
+{
+    internal interface IDatabaseProvider<T>
+    {
+        IList<T> Items { get; }
+
+        event EventHandler<DataItemEventArgs<T>> DatabaseChanged;
+
+        void AddItem(T item);
+        bool Contains(string itemName);
+        T Find(string itemName);
+        void DeleteItem(T item);
+    }
+}
